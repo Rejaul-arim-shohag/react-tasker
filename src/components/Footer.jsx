97e2d744c18fb431de2d15a64e27916444d65661
Rejaul karim <76891535+Rejaul-arim-shohag@users.noreply.github.com>
2024-01-17 10:04:38 +0600

    file structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 md:py-8">
+      <div className="container mx-auto">
+        <p className="text-center text-xs text-[#f05151] lg:text-sm">Copyright ©2024 | All rights reserved by Learn with Sumit</p>
+      </div>
+    </footer>
+  );
+}
